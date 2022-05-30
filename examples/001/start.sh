6d2ae4877ad120e3704154c13a7251775ae340bc
andytyc <andy.tengyc@gmail.com>
2022-05-30 18:17:45 +0800

    updated

--- examples/001/start.sh
+++ examples/001/start.sh
@@ -1,8 +1,12 @@
-source /code/docker-cases/demo/001/env.sh
+echo "-- Start --"
+
+source /app/env.sh
+
 echo "----"
 echo $ALG_DIR
 echo $PATH
 echo $DARWIN_ETCD_ENDPOINTS
 echo $DARWIN_ETCD_AUTH
 echo "----"
+
 /app/docker-cases
