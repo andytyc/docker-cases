2c76a5dc9599134f17fc7d089a1ba9d3f330059d
andytyc <andy.tengyc@gmail.com>
2022-05-30 17:51:47 +0800

    updated

---
+++ demoenv/01.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export ALG_DIR=/home/andytyc/docker-cases/algorithms
+export PATH=/usr/local/cuda/bin:$PATH
+
+export DARWIN_ETCD_ENDPOINTS=192.168.0.4:2379
+export DARWIN_ETCD_AUTH=root:demo666
